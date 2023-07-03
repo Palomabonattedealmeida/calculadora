57f4306d9926e773d2f0cedb390e164fdfdb817f
Paloma Almeida <palomabernardo.pbda@gmail.com>
2023-07-03 18:37:56 -0300

    calculadora

---
+++ calculadora/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculadora from './Calculadora';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculadora/>
+      <h1>Calculadora</h1>
+      
+    </div>
+  );
+}
+
+export default App;
